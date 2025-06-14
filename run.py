83012cce5a5d8ed41189e48e704e1306b5f3ffc5
Harshada_Patil <145033109+Harshadapatil24@users.noreply.github.com>
2025-06-14 10:38:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python
+"""from simple_app import app
+
+if __name__ == '__main__':
+    port = 3000
+    print(f"Starting server at http://localhost:{port}")
+    app.run(host='localhost', port=port, debug=True) """
+
+from simple_app import app
 import os
 
-port = int(os.environ.get("PORT", 5000))
-
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT from the environment
+    print(f"Starting server at http://0.0.0.0:{port}")
+    app.run(host='0.0.0.0', port=port, debug=True)
