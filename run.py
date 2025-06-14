c133a4ac9ed0dc2b4f0652921646eb97066b5a5a
Harshada_Patil <145033109+Harshadapatil24@users.noreply.github.com>
2025-06-14 10:25:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@
 from simple_app import app
 
 if __name__ == '__main__':
-    port = 3000
+    port = 5000
     print(f"Starting server at http://localhost:{port}")
     app.run(host='localhost', port=port, debug=True) 
